13b1106ea4ad025d33f56be55b7934ef196becaa
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-09 18:34:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var App = require('app'),
-    app = new App();
+var express = require('express');
+var app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
