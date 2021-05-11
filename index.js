aaa00cbfbe1cd66043c0d529094ab207b8d05f96
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-11 12:53:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ client.on("message", msg => {
     } else {
         return;
     }
-    fetch(link).then(function (response) {
-        return response.html();
-    });
-    client.channels.cache.get(active_channel).send("Testing message.", {
-      files: [ "response.html" ]
+    fetch(link){
+        .then((response) => response.html())
+        client.channels.cache.get(active_channel).send("Testing message.", {
+            files: [ "response.html" ]
+        });
     });
   };
   console.log('End');
