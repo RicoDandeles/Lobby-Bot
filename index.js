276c1fe9dde72c5c55360cd6ad8169f7961718e5
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-10 23:45:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,24 +8,34 @@ app.listen(PORT, () => {
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+// CHANGE THESE
 const discordusername = 'BOTNAME#0000'
 const discordtoken = 'bot/user token'
+const serviceFree_channel = '000000000000000000';
+const servicePaid_channel = '000000000000000000';
+const serviceDomain = 'http://www.WEBSITE.com';
+//
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", msg => {
-  var serviceFree_channel = '000000000000000000';
-  var servicePaid_channel = '000000000000000000';
-  var serviceDomain = 'http://www.WEBSITE.com';
   var messaged_channel = msg.channel.id;
   if ((messaged_channel == serviceFree_channel) || (messaged_channel == servicePaid_channel)){ 
-    var message = msg.content;
+    var link = msg.content;
     console.log('Recieved Link');
     msg.delete();
-    if (message.includes(serviceDomain)){
-        
+    if (link.includes(serviceDomain)){
+        link = link.replace("com", "club");
+        puppeteer.launch({ headless: true }).then(async browser => {
+            const page = await browser.newPage()
+            await page.goto(link)
+        });
     };
     else{
         return;
