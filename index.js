067588fac1eb116e3466ccfcd7a8d9d2ff0e54e4
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-11 00:14:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ puppeteer.use(StealthPlugin())
 // CHANGE THESE
 const discordusername = 'BOTNAME#0000'
 const discordtoken = 'ODQxNTIwMTkyMDg5NjIwNTQw.YJn8wA.u1qor9b71EDhKujXW5zSnc1b9Eg'
-const serviceFree_channel = '000000000000000000';
+const serviceFree_channel = '840515343054667807';
 const servicePaid_channel = '000000000000000000';
 const serviceDomain = 'http://www.WEBSITE.com';
 //
@@ -26,27 +26,32 @@ client.on("ready", () => {
 
 client.on("message", msg => {
   var messaged_channel = msg.channel.id;
-  if ((messaged_channel == serviceFree_channel) || (messaged_channel == servicePaid_channel)){ 
-    var link = msg.content;
-    console.log('Recieved Link');
-    msg.delete();
-    if (link.includes(serviceDomain)){
-        link = link.replace("com", "club");
-        puppeteer.launch({ headless: true }).then(async browser => {
-            const page = await browser.newPage()
-            await page.goto(link)
-            document.getElementById('download').click();
-            <a href=link download> test </a>
-        });
-        client.channels.cache.get(html_channel).send("Testing message.", {
-            files: [
-                "test",
-                    ]
-        });
-    };
-    else{
-        return;
-    };
+  if (messaged_channel == serviceFree_channel){
+      var active_channel = serviceFree_channel;
+  };
+  if (messaged_channel == servicePaid_channel){
+      var active_channel = servicePaid_channel;
+  };
+  else{
+      return;
+  };
+  var link = msg.content;
+  console.log('Recieved Link');
+  msg.delete();
+  if (link.includes(serviceDomain)){
+    link = link.replace("com", "club");
+    puppeteer.launch({ headless: true }).then(async browser => {
+        const page = await browser.newPage()
+        await page.goto(link)
+        document.getElementById('download').click();
+        <a href=link download> test </a>
+    });
+    client.channels.cache.get(active_channel).send("Testing message.", {
+      files: [ "test" ]
+    });
+  };
+  else{
+    return;
   };
 });
         
