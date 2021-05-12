9f6dba04684932226697a2aa8e04ccdd3d9919c0
Duy Pham Le <phamleduy04@gmail.com>
2021-05-11 21:55:08 -0500

    upload

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const createError = require('http-errors');
 const PORT = process.env.PORT || 3000;
-const fs  = require('fs');
-const { writeFileSync } = require('fs');
-const { rename }  = require('fs');
-const { join } = require('path');
+const { writeFileSync, unlinkSync } = require('fs');
 const cheerio = require('cheerio');
+const domain = process.env.NODE_ENV == 'production' ? 'https://botswana-brainly.herokuapp.com' : `http://localhost:${PORT}`;
+const { join } = require('path');
+const ms = require('ms');
+
+app.use(express.json());
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
- 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+
+app.use('/assets', express.static(join( __dirname, 'assets')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client();
 
 const fetch = require("node-fetch");
-const { Octokit } = require('@octokit/rest');
-const { Base64 } = require('js-base64');
-const octokit = new Octokit({
-  auth: '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',
-});
 
 // CHANGE THESE
 const discordusername = 'Brainly Bot#5119'
-const discordtoken = 'ODQxNTIwMTkyMDg5NjIwNTQw.YJn8wA.u1qor9b71EDhKujXW5zSnc1b9Eg'
+const discordtoken = 'NjY4NjEyNjAzNjY0MTM4MjYw.XiT0Ag.D2GYIQrYJYKPEV012kPwY_bR72A'
 const serviceFree_channel = '840515343054667807';
-const servicePaid_channel = '841544000422281217';
+const servicePaid_channel = '720117652432879717';
 const serviceDomain = 'brainly';
 //
 
@@ -34,10 +52,7 @@ client.on("ready", () => {
 
 client.on("message", async msg => {
   var messaged_channel = msg.channel.id;
-  var active_channel;
-  if (messaged_channel == serviceFree_channel) active_channel = serviceFree_channel;
-  else if (messaged_channel == servicePaid_channel) active_channel = servicePaid_channel;
-  else return;
+  if (![serviceFree_channel, servicePaid_channel].includes(messaged_channel)) return;
   var link = msg.content;
   console.log('Recieved Link');
   if (msg.deletable) msg.delete();
@@ -53,25 +68,24 @@ client.on("message", async msg => {
         .then(response => {
             const $ = cheerio.load(response);
             $('title').text('Homework Senpai');
-            $('alert-link').attr('href', 'https://discord.gg/XM35RczsuQ');
-            writeFileSync('./response.html', $.html().toString().replace('/logo.png', 'https://i.imgur.com/9tL2f2C.jpg'));
-            fs.renameSync('response.html', final_link);
-            githubUpload(final_link);
+            writeFileSync(`./assets/${final_link}`, $.html().toString().replace('/logo.png', 'https://i.imgur.com/9tL2f2C.jpg').replace('https://clusters.top/discord', 'https://discord.gg/XM35RczsuQ'));
             console.log('Process Finished');
-            msg.author.send({embed: {
-                color: 3447003,
-                title: "Here is your requested page.",
-                fields: [
-                    { name: "Link:", value: "https://ricodandeles.github.io/homework-senpai/homework-senpai/files/" + final_link, inline: true},
-                ]
-            }});
+            const embed = new MessageEmbed()
+              .setColor(344703)
+              .setTitle("Here is your requested page.")
+              .setDescription(`Click [here](${domain}/assets/${final_link}) to view unlocked document!`);
+            msg.author.send(embed);
+
+            // delete the file after 30min
+            setTimeout(() => {
+              unlinkSync(`./assets/${final_link}`);
+            }, ms('30m'));
         })
   };
   console.log('End');
 });
 
 function generateSerial() {
-    'use strict';
     var chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
         serialLength = 10,
         randomSerial = "",
@@ -83,25 +97,6 @@ function generateSerial() {
     }
     return randomSerial;
 };
-async function githubUpload(filename) {
-    const content = fs.readFileSync(filename, 'utf-8');
-    const fileOutput = Base64.encode(content);
 
-    const { data } = await octokit.repos.createOrUpdateFileContents({
-        owner: 'RicoDandeles',
-        repo: 'homework-senpai',
-        path: 'homework-senpai/files/' + filename,
-        message: 'Added ' + filename,
-        content: fileOutput,
-        committer: {
-            name: `RicoDandeles`,
-            email: 'rtiklegaming@gmail.com',
-        },
-        author: {
-            name: 'RicoDandeles',
-            email: 'rtiklegaming@gmail.com',
-        },
-    });
-};
-client.login(discordtoken); 
- 
+
+client.login(discordtoken);
