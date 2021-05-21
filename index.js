7c96bf506336832986227bab3b4f175677859c04
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-21 15:10:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ client.on("message", async msg => {
                     { name: "How to close your channel:", value: "When you are done, you may close a channel with `-close "+ encoded_room_name +"`.", inline: true},
                 ]
         }});
-        msg.member.add(role);
+        // msg.member.add(role);
     }
     
   }
